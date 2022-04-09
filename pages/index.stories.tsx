e306cdc6d5c6b302da9091242d2b937ed6c2d9a3
Alex Eagleson <alexeagleson@gmail.com>
2022-04-09 15:21:57 -0400

    feat: build home page

--- pages/index.stories.tsx
+++ pages/index.stories.tsx
@@ -1,17 +1,12 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import Home, { IHome } from './';
+import Home from './';
 
 export default {
   title: 'pages/Home',
   component: Home,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {},
 } as ComponentMeta<typeof Home>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Home> = (args) => <Home {...args} />;
 
 export const Base = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-
-Base.args = {} as IHome;
